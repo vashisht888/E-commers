8ab8b8f60cbb7557bb9f0e27ee9df146605777b9
vishalvashisht <vvashisht264@gmail.com>
2024-12-06 20:32:39 +0530

    first commit

---
+++ backend/db.js
@@ -0,0 +1,7 @@
+// const mongoose  = require("mongoose");
+// require('dotenv').config()
+
+// const connection = mongoose.connect(process.env.mongo_URL)
+// // const connection = mongoose.connect("mongodb+srv://vishal:<7877626231vishal>@cluster0.9fugx.mongodb.net/ecommarce?retryWrites=true&w=majority&appName=Cluster0")
+
+// module.exports = {connection}
